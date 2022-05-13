c4f251fe2e2d5bafcd1d12cc6526adf30c5575c8
turalismylv <turalhi@code.edu.az>
2022-05-13 15:31:17 +0400

    HomeWork1305

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task - 1
+            //IsPrime metodu -parametr olaraq ineteger value qebul edir
+            //.Gonderilims integer deyerin sade olub olmadigini qaytarir.Eded sadedirse true, deyilse false qayitmalidir
+
+            //    IsPrime(5);
+            //}
+
+            //static void IsPrime(int a) {
+
+            //    if (a == 1) {
+            //        Console.WriteLine("ne sade nede murekkebdir");
+            //    }
+            //    for (int i = 2; i < a; i++)
+            //    {
+            //        if (a % i == 0)
+            //        {
+            //            Console.WriteLine("False");
+            //            break;
+            //        }
+            //        else {
+            //            Console.WriteLine("True");
+            //            break;
+            //        }
+            //    }
+
+
+            //            Task - 2
+            //CalcAvg metodu -Gondeirlmis imtahanlar qiymeti siyahisina(integer array) esasen hemin qiymetlerin ortalamasini qaytaran metod yazin.
+            //Main metodunda bu metodu call edib, burdan qayidan deyere esasen ortalama 60 - dan boyukdursa console-da "mezun oldunuz" eks halda "mezun ola bilmediniz" yazisi yazdirin
+
+        //    int[] arr = {60,80,100 };
+        //    int a = CalcAvg(arr);
+        //    if (a > 60)
+        //    {
+        //        Console.WriteLine("mezun oldunuz");
+        //    }
+        //    else if (a <= 60)
+        //    {
+
+        //        Console.WriteLine("mezun ola bilmediniz");
+        //    }
+
+
+        //}
+        //static int CalcAvg(int[] arr)
+        //{
+
+
+        //    int counter = 0;
+        //    int sum = 0;
+
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+
+        //        sum += arr[i];
+        //        counter++;
+
+        //    }
+        //    int result = sum / counter;
+        //    return result;
+        } 
+         }
+        }
+    
+
+			
+
+
+
+        
+    
